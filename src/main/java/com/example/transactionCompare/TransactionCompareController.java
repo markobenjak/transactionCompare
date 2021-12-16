7c09bbe2fcbf7b940ab6faa422640545bf20fec9
mbenjak96 <marko.benjak@tvz.hr>
2021-12-16 08:02:32 +0100

    Added endpoint response and most of the frontend

--- src/main/java/com/example/transactionCompare/TransactionCompareController.java
+++ src/main/java/com/example/transactionCompare/TransactionCompareController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.util.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class TransactionCompareController {
