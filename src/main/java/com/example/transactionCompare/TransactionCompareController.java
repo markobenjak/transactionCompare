f4f2e22bc392da159e17e999c21558d1fce39c76
mbenjak96 <marko.benjak@tvz.hr>
2021-12-18 15:41:10 +0100

    Update tests

--- src/main/java/com/example/transactionCompare/TransactionCompareController.java
+++ src/main/java/com/example/transactionCompare/TransactionCompareController.java
@@ -3,6 +3,7 @@ package com.example.transactionCompare;
 import com.example.transactionCompare.FileModel.ClientProfile;
 import com.example.transactionCompare.FileModel.ReturnGenericResponse;
 import com.example.transactionCompare.FileModel.ReturnResultData;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,90 +39,113 @@ public class TransactionCompareController {
     // Post request for files upload
     @PostMapping("/uploadFiles")
     public Object uploadFiles(@RequestParam("csvFile") MultipartFile csvFile, @RequestParam("csvFile2") MultipartFile csvFile2) throws IOException {
-        try {
-            HashMap<Integer, String> file1Map = new HashMap<Integer, String>(); // HashMap which contains parsed data from file 1
-            List<ClientProfile> tmpClientProfileListFile1 = new ArrayList<>(); // Temporary List of objects from File 1
-            List<ClientProfile> tmpClientProfileListFile2 = new ArrayList<>(); // Temporary List of objects from File 2
+        System.out.println(FilenameUtils.getExtension(csvFile.getOriginalFilename()));
+        if (csvFile == null) {
+            ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
+            returnGenericResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+            returnGenericResponse.setResponseMessage("File 1 missing");
+            return returnGenericResponse;
+        } else if (csvFile2 == null) {
+            ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
+            returnGenericResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+            returnGenericResponse.setResponseMessage("File 2 missing");
+            return returnGenericResponse;
+        } else if(!FilenameUtils.getExtension(csvFile.getOriginalFilename()).equals("csv")){
+            ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
+            returnGenericResponse.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+            returnGenericResponse.setResponseMessage("File 1 wrong Format");
+            return returnGenericResponse;
+        }else if(!FilenameUtils.getExtension(csvFile2.getOriginalFilename()).equals("csv")){
+            ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
+            returnGenericResponse.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+            returnGenericResponse.setResponseMessage("File 2 wrong Format");
+            return returnGenericResponse;
+        }else {
+            try {
+                HashMap<Integer, String> file1Map = new HashMap<Integer, String>(); // HashMap which contains parsed data from file 1
+                List<ClientProfile> clientProfileListFile1 = new ArrayList<>(); // List of objects from File 1
+                List<ClientProfile> clientProfileListFile2 = new ArrayList<>(); // List of objects from File 2
 
-            // Reading File 1
-            byte[] csvFileBytes = csvFile.getBytes();
-            ByteArrayInputStream inputCsvFileStream = new ByteArrayInputStream(csvFileBytes);
-            BufferedReader brFile = new BufferedReader(new InputStreamReader(inputCsvFileStream ));
-            String csvFileLines = "";
-            Integer numberOfFile1Lines = 0;
-            while ((csvFileLines = brFile.readLine()) != null) {
-                file1Map.put(numberOfFile1Lines, csvFileLines); // Placing values inside HashMap
-                numberOfFile1Lines++;
-            }
-            brFile.close(); // Closing buffer reader
+                // Reading File 1
+                byte[] csvFileBytes = csvFile.getBytes();
+                ByteArrayInputStream inputCsvFileStream = new ByteArrayInputStream(csvFileBytes);
+                BufferedReader brFile = new BufferedReader(new InputStreamReader(inputCsvFileStream));
+                String csvFileLines = "";
+                Integer numberOfFile1Lines = 0;
+                while ((csvFileLines = brFile.readLine()) != null) {
+                    file1Map.put(numberOfFile1Lines, csvFileLines); // Placing values inside HashMap
+                    numberOfFile1Lines++;
+                }
+                brFile.close(); // Closing buffer reader
 
-            // Reading File 2
-            byte[] csvFile2Bytes = csvFile2.getBytes();
-            ByteArrayInputStream inputCsvFile2Stream = new ByteArrayInputStream(csvFile2Bytes);
-            BufferedReader brFile2 = new BufferedReader(new InputStreamReader(inputCsvFile2Stream ));
-            String csvFile2Lines = "";
-            Integer numberOfFile2Lines = 0;
-            while ((csvFile2Lines = brFile2.readLine()) != null) {
-                if (file1Map.containsValue(csvFile2Lines)) {
-                    // If File 1 and 2 have matching records
-                    for (Object o : file1Map.keySet()) {
-                        if (file1Map.get(o).equals(csvFile2Lines)) {
-                            file1Map.remove(o); // Removing matching Records from File 1 HashMap
-                            break;
+
+                // Reading File 2
+                byte[] csvFile2Bytes = csvFile2.getBytes();
+                ByteArrayInputStream inputCsvFile2Stream = new ByteArrayInputStream(csvFile2Bytes);
+                BufferedReader brFile2 = new BufferedReader(new InputStreamReader(inputCsvFile2Stream));
+                String csvFile2Lines = "";
+                Integer numberOfFile2Lines = 0;
+                while ((csvFile2Lines = brFile2.readLine()) != null) {
+                    if (file1Map.containsValue(csvFile2Lines)) {
+                        // If File 1 and 2 have matching records
+                        for (Object o : file1Map.keySet()) {
+                            if (file1Map.get(o).equals(csvFile2Lines)) {
+                                file1Map.remove(o); // Removing matching Records from File 1 HashMap
+                                break;
+                            }
+                        }
+                    } else {
+                        //if File 1 does not contain the same record as File 2
+                        String[] splitCsvLineFile2 = csvFile2Lines.split(","); // Splitting row into Object data
+                        String[] splitCsvLineFile2FinalSize = new String[8]; // Creating new string array so that rows which are missing data do not break the application
+                        System.arraycopy(splitCsvLineFile2, 0, splitCsvLineFile2FinalSize, 0, splitCsvLineFile2.length);
+                        ClientProfile clientProfileFile2 = new ClientProfile(splitCsvLineFile2FinalSize);
+                        clientProfileListFile2.add(clientProfileFile2); // Adding objects to object list
+                    }
+                    numberOfFile2Lines++;
+                }
+                brFile2.close(); // Closing buffer reader for file 2
+
+                for (Map.Entry<Integer, String> entry : file1Map.entrySet()) { // Iterating through HashMap of File 1
+                    String[] splitCsvLineFile1 = entry.getValue().split(","); // Splitting each row
+                    String[] splitCsvLineFile1FinalSize = new String[8]; // Creating new string array so that rows which are missing data do not break the application
+                    System.arraycopy(splitCsvLineFile1, 0, splitCsvLineFile1FinalSize, 0, splitCsvLineFile1.length);
+                    ClientProfile clientProfileFile1 = new ClientProfile(splitCsvLineFile1FinalSize);
+                    clientProfileListFile1.add(clientProfileFile1); // Adding objects to object list
+                }
+
+                // Matching records with the same date
+                int connectionValue = 0;
+                for (ClientProfile clientProfileCloseMatchesFile1 : clientProfileListFile1) {
+                    for (ClientProfile clientProfileCloseMatchesFile2 : clientProfileListFile2) {
+                        if (clientProfileCloseMatchesFile1.getTransactionDate().equals(clientProfileCloseMatchesFile2.getTransactionDate())) {
+                            clientProfileCloseMatchesFile1.setConnection(connectionValue);
+                            clientProfileCloseMatchesFile2.setConnection(connectionValue);
                         }
                     }
-                }else {
-                    //if File 1 does not contain the same record as File 2
-                    String[] splitCsvLineFile2 = csvFile2Lines.split(","); // Splitting row into Object data
-                    String[] splitCsvLineFile2FinalSize = new String[8]; // Creating new string array so that rows which are missing data do not break the application
-                    System.arraycopy(splitCsvLineFile2, 0, splitCsvLineFile2FinalSize, 0, splitCsvLineFile2.length);
-                    ClientProfile clientProfileFile2 = new ClientProfile(splitCsvLineFile2FinalSize);
-                    tmpClientProfileListFile2.add(clientProfileFile2); // Adding objects to object list
+                    connectionValue++;
                 }
-                numberOfFile2Lines++;
-            }
-            brFile2.close(); // Closing buffer reader for file 2
 
-            for(Map.Entry<Integer, String> entry : file1Map.entrySet()) { // Iterating through HashMap of File 1
-                String[] splitCsvLineFile1 = entry.getValue().split(","); // Splitting each row
-                String[] splitCsvLineFile1FinalSize = new String[8]; // Creating new string array so that rows which are missing data do not break the application
-                System.arraycopy(splitCsvLineFile1, 0, splitCsvLineFile1FinalSize, 0, splitCsvLineFile1.length);
-                ClientProfile clientProfileFile1 = new ClientProfile(splitCsvLineFile1FinalSize);
-                tmpClientProfileListFile1.add(clientProfileFile1); // Adding objects to object list
-            }
+                // Sorting lists based on date field
+                Collections.sort(clientProfileListFile1, comparing(ClientProfile::getTransactionDate));
+                Collections.sort(clientProfileListFile2, comparing(ClientProfile::getTransactionDate));
 
-            List<ClientProfile> clientProfileListFile1 = new ArrayList<>(); // Temporary List of objects from File 1
-            List<ClientProfile> clientProfileListFile2 = new ArrayList<>(); // Temporary List of objects from File 2
-            int connectionValue = 0;
-            for(ClientProfile clientProfileCloseMatchesFile1: tmpClientProfileListFile1){
-                for(ClientProfile clientProfileCloseMatchesFile2: tmpClientProfileListFile2) {
-                    if (clientProfileCloseMatchesFile1.getTransactionDate().equals(clientProfileCloseMatchesFile2.getTransactionDate())) {
-                        clientProfileCloseMatchesFile1.setConnection(connectionValue);
-                        clientProfileCloseMatchesFile2.setConnection(connectionValue);
-                        clientProfileListFile1.add(clientProfileCloseMatchesFile1);
-                        clientProfileListFile2.add(clientProfileCloseMatchesFile2);
-                    }
-                }
-                connectionValue++;
+                ReturnResultData returnResultData = new ReturnResultData(); // Object which is returned as response in case of success
+                returnResultData.setTotalNumberFile1(numberOfFile1Lines);
+                returnResultData.setTotalNumberFile2(numberOfFile2Lines);
+                returnResultData.setClientProfileFile1(clientProfileListFile1);
+                returnResultData.setClientProfileFile2(clientProfileListFile2);
+                returnResultData.setUnmatchedRecordsFile1(clientProfileListFile1.size());
+                returnResultData.setUnmatchedRecordsFile2(clientProfileListFile2.size());
+                returnResultData.setStatus(HttpStatus.OK.value());
+                returnResultData.setResponseMessage("SUCCESS");
+                return returnResultData;
+            } catch (Exception ex) {
+                ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
+                returnGenericResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                returnGenericResponse.setResponseMessage(ex.getLocalizedMessage());
+                return returnGenericResponse;
             }
-            Collections.sort(tmpClientProfileListFile1, comparing(ClientProfile::getTransactionDate));
-            Collections.sort(tmpClientProfileListFile2, comparing(ClientProfile::getTransactionDate));
-
-            ReturnResultData returnResultData = new ReturnResultData(); // Object which is returned as response in case of success
-            returnResultData.setTotalNumberFile1(numberOfFile1Lines);
-            returnResultData.setTotalNumberFile2(numberOfFile2Lines);
-            returnResultData.setClientProfileFile1(tmpClientProfileListFile1);
-            returnResultData.setClientProfileFile2(tmpClientProfileListFile2);
-            returnResultData.setUnmatchedRecordsFile1(tmpClientProfileListFile1.size());
-            returnResultData.setUnmatchedRecordsFile2(tmpClientProfileListFile2.size());
-            returnResultData.setStatus(HttpStatus.OK.value());
-            returnResultData.setResponseMessage("SUCCESS");
-            return returnResultData;
-        }catch (Exception ex){
-            ReturnGenericResponse returnGenericResponse = new ReturnGenericResponse(); // Object which is returned in case of error
-            returnGenericResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            returnGenericResponse.setResponseMessage(ex.getLocalizedMessage());
-            return returnGenericResponse;
         }
     }
 }
--- src/test/java/com/example/transactionCompare/TransactionCompareApplicationTests.java
+++ src/test/java/com/example/transactionCompare/TransactionCompareApplicationTests.java
@@ -1,29 +1,97 @@
 package com.example.transactionCompare;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.InputStream;
 
-@SpringBootTest
-class TransactionCompareApplicationTests {
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TransactionCompareController.class)
+public class TransactionCompareApplicationTests {
 
 	@Autowired
-	private TransactionCompareController transactionCompareController;
+	private WebApplicationContext webApplicationContext;
 	@Autowired
-	private TestRestTemplate restTemplate;
+	private MockMvc mockMvc;
 
 	@Test
-	public void contextLoads() throws Exception {
-		assertThat(transactionCompareController).isNotNull();
+	public void uploadBothFiles() throws Exception {
+		InputStream uploadStream = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file = new MockMultipartFile("csvFile", uploadStream);
+		InputStream uploadStream2 = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file2 = new MockMultipartFile("csvFile2", uploadStream2);
+
+		this.mockMvc
+				.perform(multipart("/api/uploadFiles")
+						.file(file)
+						.file(file2))
+				.andExpect(status().isOk());
+
 	}
 
 	@Test
-	public void greetingShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + 7777 + "/",
-				String.class)).contains("Hello, World");
+	public void uploadFirstFileOnly() throws Exception {
+		InputStream uploadStream = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file = new MockMultipartFile("csvFile", uploadStream);
+
+		this.mockMvc
+				.perform(multipart("/api/uploadFiles")
+						.file(file))
+				.andExpect(status().isBadRequest());
+
 	}
 
+	@Test
+	public void uploadSecondFileOnly() throws Exception {
+		InputStream uploadStream = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file = new MockMultipartFile("csvFile2", uploadStream);
+
+		this.mockMvc
+				.perform(multipart("/api/uploadFiles")
+						.file(file))
+				.andExpect(status().isBadRequest());
+
+	}
+
+	@Test
+	public void wrongFormatFile1() throws Exception {
+		InputStream uploadStream = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.txt");
+		MockMultipartFile file = new MockMultipartFile("csvFile", uploadStream);
+		InputStream uploadStream2 = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file2 = new MockMultipartFile("csvFile2", uploadStream2);
+
+		this.mockMvc
+				.perform(multipart("/api/uploadFiles")
+						.file(file)
+						.file(file2))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.responseMessage").value("File 1 wrong Format"));
+	}
+
+	@Test
+	public void wrongFormatFile2() throws Exception {
+		InputStream uploadStream = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.csv");
+		MockMultipartFile file = new MockMultipartFile("csvFile", uploadStream);
+		InputStream uploadStream2 = TransactionCompareApplicationTests.class.getClassLoader().getResourceAsStream("exceldocument.txt");
+		MockMultipartFile file2 = new MockMultipartFile("csvFile2", uploadStream2);
+
+		this.mockMvc
+				.perform(multipart("/api/uploadFiles")
+						.file(file)
+						.file(file2))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.responseMessage").value("File 2 wrong Format"));
+	}
 }
